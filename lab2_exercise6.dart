8f5802eb638f5c6d98899b1f5c5a546f0ba46fa6
Teenawat <hirunthunpreedaphu@gmail.com>
2026-07-01 11:53:36 +0700

    lol

---
+++ lab2_exercise6.dart
@@ -0,0 +1,12 @@
+void greet(String name) {
+  print('Hello, $name!');
+  int multiply(int a, int b) => a * b;
+
+  double calculateArea({required double width, required double height}) 
+  {
+    return width * height;
+  }
+  void main() {
+    greet('Teenawat')
+  }
+}
